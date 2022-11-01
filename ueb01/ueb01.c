98f165085d006fc8e6cc6ce0f37325e06a14f1bc
cgt104277 <cgt104277@b38374a9-ca3c-413a-9549-de45cfa5720b>
2022-11-01 13:20:35 +0000

    devision wird jetzt richtig ausgegeben bei ZAHL 1

--- ueb01/ueb01.c
+++ ueb01/ueb01.c
@@ -19,27 +19,27 @@
  */
 void
 printUsage  (FILE * stream) {
-  fprintf(stream, "Usage:\n\n");
-  
-  fprintf(stream, "  ueb01 -h\n\n");
-  
-  fprintf(stream, "  shows this help and exits.\n\n");
-  
-  fprintf(stream, "  - or -\n\n");
-  
-  fprintf(stream, "  ueb01 NUM OP\n\n");
-  
-  fprintf(stream, "  checks whether a given number is prime or a happy number or finds the\n");
-  fprintf(stream, "  divisors of a given number. A call always consists of a number followed\n");
-  fprintf(stream, "  by an operation, where:\n\n");
-  
-  fprintf(stream, "  NUM has to be a positive integer number (including 0).\n\n");
-  
-  fprintf(stream, "  OP is one of:\n");
-  fprintf(stream, "    p checks whether NUM is prime or not\n");
-  fprintf(stream, "    d looks for the dividers of NUM\n");
-  fprintf(stream, "    h checks whether NUM is a happy number\n");
-  
+	fprintf(stream, "Usage:\n\n");
+	
+	fprintf(stream, "  ueb01 -h\n\n");
+	
+	fprintf(stream, "  shows this help and exits.\n\n");
+	
+	fprintf(stream, "  - or -\n\n");
+	
+	fprintf(stream, "  ueb01 NUM OP\n\n");
+	
+	fprintf(stream, "  checks whether a given number is prime or a happy number or finds the\n");
+	fprintf(stream, "  divisors of a given number. A call always consists of a number followed\n");
+	fprintf(stream, "  by an operation, where:\n\n");
+	
+	fprintf(stream, "  NUM has to be a positive integer number (including 0).\n\n");
+	
+	fprintf(stream, "  OP is one of:\n");
+	fprintf(stream, "    p checks whether NUM is prime or not\n");
+	fprintf(stream, "    d looks for the dividers of NUM\n");
+	fprintf(stream, "    h checks whether NUM is a happy number\n");
+	
 }
 
 /**
@@ -49,10 +49,11 @@ typedef enum{
 	ERR_None,
 	ERR_NoParameters,
 	ERR_WrongHelp,
-	ERR_TooMuchInformation,
-	ERR_NoPositivNumber,
 	ERR_WrongNumber,
-	ERR_WrongOperator
+	ERR_WrongOperatorFormat,
+	ERR_NoPositivNumber,
+	ERR_WrongOperator,
+	ERR_TooMuchInformation
 }Error;
 
 Error
@@ -60,17 +61,19 @@ errorHandling(Error err){
 	switch(err) {
 		case ERR_None:
 		break;
-		case ERR_NoParameters:			fprintf(stderr, "%s\n", "Error: No parameters!");
+		case ERR_NoParameters: fprintf(stderr, "%s\n", "Error: No parameters!");
 		break;
-		case ERR_WrongHelp: 			fprintf(stderr, "%s\n", "Error: Wrong help call!");
+		case ERR_WrongHelp: fprintf(stderr, "%s\n", "Error: Wrong help call!");
 		break;
-		case ERR_TooMuchInformation: 	fprintf(stderr, "%s\n", "Error: Too much information!");
+		case ERR_WrongNumber: fprintf(stderr, "%s\n", "Error: Number must be integer!");
 		break;
-		case ERR_NoPositivNumber: 		fprintf(stderr, "%s\n", "Error: Number must be positive!");
+		case ERR_WrongOperatorFormat: fprintf(stderr, "%s\n", "Error: Wrong format for operator!");
 		break;
-		case ERR_WrongNumber: 			fprintf(stderr, "%s\n", "Error: Number must be integer!");
+		case ERR_NoPositivNumber: fprintf(stderr, "%s\n", "Error: Number must be positive!");
 		break;
-		case ERR_WrongOperator: 		fprintf(stderr, "%s\n", "Error: Wrong Operator!");
+		case ERR_WrongOperator: fprintf(stderr, "%s\n", "Error: Wrong Operator!");
+		break;
+		case ERR_TooMuchInformation: fprintf(stderr, "%s\n", "Error: Too much information!");
 		break;
 	}
 	if (err != ERR_None) {
@@ -87,8 +90,8 @@ isHappy(int number){
 		temp = 0,
 		copyNum = number,
 		sum = 0;
-	
-	
+
+
 	while(sum != 1 && sum != 4){
 		sum = 0;
 		//Einzelne Positions- und Teilsummenberechnung
@@ -117,29 +120,25 @@ isHappy(int number){
 int 
 isPrime (int number){
 	int primNum = 1, //Erste Annahme eine Primzahl
-	    count = 2;
-
+		count = 2;
+	
 	//Die Kleinste Primzahl ist 2
 	if (number <= 1) {
 		primNum = 0;
-	} else {
-		if (number == 2) {
-			primNum = 1;
-		} else {
-			//Prüfung auf ungerade Zahlen	
-			if (number % count == 1){
-				count++;
-				//Berechnung ob die Zahl teilbar durch das Quadrat von count ist
-				while ((primNum == 1) && ((count * count) <= number)) {
-					if (number % count == 0) {
-						primNum = 0;
-					}
-					count += 2;
-				}
-			} else {
+	} else if (number == 2) {
+		primNum = 1;
+	//Prüfung auf ungerade Zahlen
+	} else if (number % count == 1){
+		count++;
+		//Berechnung ob die Zahl teilbar durch das Quadrat von count ist
+		while ((primNum == 1) && ((count * count) <= number)) {
+			if (number % count == 0) {
 				primNum = 0;
 			}
+			count += 2;
 		}
+	} else {
+		primNum = 0;
 	}
 	return primNum;
 }
@@ -167,15 +166,15 @@ int
 main (int argc, char * argv[]) {
 	Error err = ERR_None;
 	char op = '\0', 
-	     END = '\0';
+		 END = '\0';
 	int divisor = 2,
-	    smallestDiv = 2,
-	    number = 0;
+		smallestDiv = 2,
+		number = 0;
 
 	switch(argc) {
 		case 0:
 		case 1: 		
-		err = ERR_NoParameters;
+			err = ERR_NoParameters;
 		break;
 		case 2:
 			/* Prüfung auf die korrekte Eingabe des Hilfeaufrufs. */
@@ -189,13 +188,19 @@ main (int argc, char * argv[]) {
 			if(sscanf(argv[1], "%i%c", &number, &END) != 1){
 				err = ERR_WrongNumber;
 			} else if(sscanf(argv[2], "%c%c", &op, &END) != 1){
-				err = ERR_WrongOperator;
+				err = ERR_WrongOperatorFormat;
 			} else if (number < 0) {
-				err = ERR_WrongOperator;
+				err = ERR_NoPositivNumber;
 			} else {
 				switch(op){
 					case 'd':
-						if(number != 0){
+						if(number == 0){
+							fprintf(stdout, "%s%d%s\n", "The number ", number,
+											" is not dividable.");
+						} else if(number == 1){
+							fprintf(stdout, "%s%d%s", "The number ", number,
+											" has the following divisors: 1\n");
+						} else {
 							fprintf(stdout, "%s%d%s", "The number ", number,
 											" has the following divisors: 1, ");
 							if(!isPrime(number)){
@@ -214,27 +219,24 @@ main (int argc, char * argv[]) {
 								}
 							}
 							fprintf(stdout, "%d\n",number);
-						} else {
-							fprintf(stdout, "%s%d%s\n", "The number ", number,
-											" is not dividable.");
 						}
 					break;
 					case 'h':
-						if(isHappy(number)){
-							fprintf(stdout, "%s%d%s\n", "*\\o/* The number ", number, 
-											" is a happy number. *\\o/*");
-						} else {
+						if(!isHappy(number)){
 							fprintf(stdout, "%s%d%s\n", "O_o The number ", number,
 											" is a sad number. o_O");
+						} else {
+							fprintf(stdout, "%s%d%s\n", "*\\o/* The number ", number, 
+											" is a happy number. *\\o/*");
 						}
 					break;
 					case 'p':
-						if(isPrime(number)){
-							fprintf(stdout, "%s%d%s\n", "*\\o/* The number ", number, 
-													" is a prime number. *\\o/*");
-						} else {
+						if(!isPrime(number)){
 							fprintf(stdout, "%s%d%s\n", "O_o The number ", number,
 											" is not a prime number. o_O");
+						} else {
+							fprintf(stdout, "%s%d%s\n", "*\\o/* The number ", number, 
+											" is a prime number. *\\o/*");
 						}
 					break;
 					default:
