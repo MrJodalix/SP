12456754fff8df1ffd341a987868b762c6141c86
cgt104277 <cgt104277@b38374a9-ca3c-413a-9549-de45cfa5720b>
2022-11-11 12:25:38 +0000

    ueb02 erste Version fertig. cipher angefangen

--- ueb02/cipher.c
+++ ueb02/cipher.c
@@ -3,7 +3,7 @@
  *
  * Implementation des Moduls mit Verschluesselungsfunktionen.
  *
- * @author mhe, mre, tti, <TODO Eure Namen ergaenzen>
+ * @author mhe, mre, tti, Ilana Schmara, Joshua-Scott Schoettke, Gruppe 21
  *
  */
 
@@ -11,40 +11,134 @@
 #include <assert.h>
 #include "cipher.h"
 
-
+/**
+ * Verschluesselt einen Text mit Caesar-Chiffre, dabei wird der uebergebene
+ * Text veraendert.
+ *
+ * @param[in]  plain Klartext
+ * @param[out] plain Kryptotext
+ * @param      rot   Rotation
+ *
+ * @pre plain != NULL
+ *
+ */
 void
 caesarEncode(Text plain, unsigned int rot) {
+	assert(plain != NULL);
   /** @TODO Implementation */
 }
 
+/**
+ * Entschluesselt einen Text mit Caesar-Chiffre, dabei wird der uebergebene
+ * Text veraendert.
+ *
+ * @param[in]  plain Kryptotext
+ * @param[out] plain Klartext
+ * @param      rot Rotation
+ *
+ * @pre plain != NULL
+ *
+ */
 void
 caesarDecode(Text plain, unsigned int rot) {
+	assert(plain != NULL);
   /** @TODO Implementation */
 }
 
+/**
+ * Verschluesselt einen Text mit Vigenere-Chiffre, dabei wird der uebergebene
+ * Text veraendert.
+ *
+ * @param[in]  plain Klartext
+ * @param[out] plain Kryptotext
+ * @param[in]  pass  Passwort
+ *
+ * @pre plain != NULL
+ * @pre pass != NULL
+ * @pre pass ist nicht leer
+ *
+ */
 void
 vigenereEncode(Text plain, Text pass) {
+	assert((plain != NULL) && (pass != NULL) && (sizeof(*pass) != 0));
   /** @TODO Implementation */
 }
 
+/**
+ * Entschluesselt einen Text mit Vigenere-Chiffre, dabei wird der uebergebene
+ * Text veraendert.
+ *
+ * @param[in]  plain Kryptotext
+ * @param[out] plain Klartext
+ * @param[in]  pass  Passwort
+ *
+ * @pre plain != NULL
+ * @pre pass != NULL
+ * @pre pass ist nicht leer
+ *
+ */
 void
 vigenereDecode(Text plain, Text pass) {
+	assert((plain != NULL) && (pass != NULL) && (sizeof(*pass) >= 1));
   /** @TODO Implementation */
 }
 
+/**
+ * Liest einen Text ein.
+ *
+ * @param[out] text eingelesener Text
+ * @param[in]  str  Eingabestring
+ *
+ * @pre text != NULL
+ * @pre str != NULL
+ *
+ * @return Fehlercode:
+ *         ERR_TEXT_ILLEGAL_CHAR wenn der einzulesende Text ungueltige Zeichen enthaelt
+ *         ERR_TEXT_TOO_LONG wenn der Text laenger ist als der verfuegbare Platz
+ */
 ErrorCode
 readText(Text text, char * str) {
+	assert((text != NULL) && (str != NULL));
   /** @TODO Implementation */
+	return ERR_NULL;
 }
 
+/**
+ * Schreibt den uebergebenen Text auf den uebergebenen Ausgabestrom.
+ *
+ * @param[in] stream Ausgabestrom
+ * @param[in] text   Text
+ *
+ * @pre stream != NULL
+ * @pre text != NULL
+ *
+ */
 void
 printText(FILE * stream, Text text) {
-  /** @TODO Implementation */
+	assert((text != NULL) && (stream != NULL));
+	fprintf(stream,"%s\n", text);
 }
 
-
+/**
+ * Gibt das Histogram des uebergebenen Texts auf dem uebergebenen Ausgabestrom aus.
+ *
+ * Die Ausgabe erfolgt spaltenweise in zwei Zeilen. Eine Spalte beinhaltet
+ * stets das im Text vorhandene Zeichen (Auftrittshaeufigkeit > 0) und die
+ * Anzahl seiner Vorkommen. Die Inhalte der Spalten sind voneinander durch
+ * Leerzeichen getrennt und rechtsbuendig ausgerichtet. Die Breite der Anzahl
+ * der Vorkommen bestimmt die Breite der Spalte. Die Zeichen werden gemaess
+ * ihrer Reihenfolge im Zeichenvorrat ausgegeben.
+ *
+ * @param[in] stream Ausgabestrom
+ * @param[in] text   Text
+ *
+ * @pre stream != NULL
+ * @pre text != NULL
+ *
+ */
 void
 printHistogram (FILE * stream, Text text) {
+	assert((text != NULL) && (stream != NULL));
   /** @TODO Implementation */
 }
 
--- ueb02/ueb02.c
+++ ueb02/ueb02.c
@@ -31,23 +31,31 @@ printUsage(FILE * stream) {
   fprintf(stream, "- or -\n\n");
   fprintf(stream, "ueb02 OPERATION [ARGUMENT] [OPERATION [ARGUMENT], ...]\n\n");
   fprintf(stream, "  Executes a number of (simple cryptographic) operations. The maximum text\n");
-  fprintf(stream, "  size for plain / cipher text and key is %i. The allowed input characters\n", MAX_TEXT_LENGTH );
+  fprintf(stream, "  size for plain / cipherCode text and key is %i. The allowed input characters\n", MAX_TEXT_LENGTH );
   fprintf(stream, "  range from \"%c\" to \"%c\".\n\n", SMALLEST_CHAR, BIGGEST_CHAR);
   fprintf(stream, "  OPERATION:\n");
-  fprintf(stream, "    -t TEXT:  Sets the TEXT as new (plain/cipher) text.\n");
-  fprintf(stream, "    -C ROT:   Sets the cipher algorithm to caesar.\n");
+  fprintf(stream, "    -t TEXT:  Sets the TEXT as new (plain/cipherCode) text.\n");
+  fprintf(stream, "    -C ROT:   Sets the cipherCode algorithm to caesar.\n");
   fprintf(stream, "              ROT is the rotation (a positive integer).\n");
-  fprintf(stream, "    -V KEY:   Sets the cipher algorithm to vigenere.\n");
+  fprintf(stream, "    -V KEY:   Sets the cipherCode algorithm to vigenere.\n");
   fprintf(stream, "              KEY is the nonempty passkey.\n");
   fprintf(stream, "    -e:       Encodes and replaces the current text using the current\n");
-  fprintf(stream, "              cipher algorithm.\n");
+  fprintf(stream, "              cipherCode algorithm.\n");
   fprintf(stream, "    -d:       Decodes and replaces the current text using the current\n");
-  fprintf(stream, "              cipher algorithm.\n");
+  fprintf(stream, "              cipherCode algorithm.\n");
   fprintf(stream, "    -i:       Writes the histogram corresponding to the current text to\n");
   fprintf(stream, "              stdout, followed by a line break.\n");
   fprintf(stream, "    -w:       Writes the current text to stdout, followed by a line break.\n");
 }
 
+
+
+typedef
+enum {
+	NO_CIPHER = 0,
+	CAESAR = 1,
+	VIGENERE = 2
+}Cipher;
 
 /**
  * Hauptprogramm.
@@ -64,13 +72,90 @@ printUsage(FILE * stream) {
 int
 main(int argc, char * argv[]) {
 
-  ErrorCode
-      error = ERR_NULL
-    ;
-  
-  if(argc < 2){
-    error = ERR_CALL_MISSING_ARGS
-  }
-
-  return error;
+	ErrorCode
+		error = ERR_NULL
+	;
+	
+	Text text = EMPTY_TEXT;
+	
+	char *readStr = "\0",
+		 *Key = "\0";
+	
+	int	idx = 1,
+		ROT = 0;
+					
+	char	OP = '\0',
+			END = '\0';
+			
+	Cipher code = NO_CIPHER;
+	
+	if(argc < 2){
+		error = ERR_CALL_MISSING_ARGS;
+	} else {
+		while((error == ERR_NULL) && (argv[idx] != NULL)){
+			if(sscanf(argv[idx],"-%c%c", &OP, &END) != 1){
+				error = ERR_UNKNOWN_OPERATION;
+			} else {
+				switch(OP){
+					case 'h':
+						if(argc == 2){
+							printUsage(stdout);
+						} else {
+							error = ERR_UNKNOWN_OPERATION;
+						}
+					break;
+					case 't':
+						idx++;
+						if(sscanf(argv[idx],"%s%c", readStr, &END) == 1){
+							error = readText(text, readStr);
+						} else {
+							error = ERR_UNKNOWN_OPERATION;
+						}
+					break;
+					case 'C':
+						idx++;
+						code = CAESAR;
+						if(sscanf(argv[idx],"%d%c", &ROT, &END) != 1){
+							error = ERR_UNKNOWN_OPERATION;
+						}
+					break;
+					case 'V':
+						idx++;
+						code = VIGENERE;
+						if(sscanf(argv[idx],"%s%c", Key, &END) != 1){
+							error = ERR_UNKNOWN_OPERATION;
+						}
+					break;
+					case 'e':
+						if(code == NO_CIPHER){
+							error = ERR_ENCODE_WITHOUT_CIPHER;
+						} else if(code == CAESAR){
+							caesarEncode(text,ROT);
+						} else if(code == VIGENERE){
+							vigenereEncode(text,Key);
+						}
+					break;
+					case 'd':
+						if(code == NO_CIPHER){
+							error = ERR_ENCODE_WITHOUT_CIPHER;
+						} else if(code == CAESAR){
+							caesarDecode(text,ROT);
+						} else if(code == VIGENERE){
+							vigenereDecode(text,Key);
+						}
+					break;
+					case 'i':
+						printHistogram(stdout,text);
+					break;
+					case 'w':
+						printText(stdout,text);
+					break;
+					default:
+						error = ERR_UNKNOWN_OPERATION;
+				}
+			}
+			idx++;
+		}
+	}
+	return error;
 }
